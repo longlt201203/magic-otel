8a2f1c50ef8ace9ef594703249b80b402460a349
Le Thanh Long <longlt201203@gmail.com>
2026-03-11 15:42:16 +0700

    feat: update

--- src/class-tracing.decorator.ts
+++ src/class-tracing.decorator.ts
@@ -1,37 +1,50 @@
-import { trace } from "@opentelemetry/api";
+import { SpanStatusCode, trace } from "@opentelemetry/api";
 
 export function ClassTracing(): ClassDecorator {
     return (target) => {
         const className = target.name;
         const tracer = trace.getTracer(className);
-        Object.getOwnPropertyNames(target.prototype).forEach((propertyKey) => {
+        const propertyKeys = [
+            ...Object.getOwnPropertyNames(target.prototype),
+            ...Object.getOwnPropertySymbols(target.prototype),
+        ];
+
+        propertyKeys.forEach((propertyKey) => {
+            if (propertyKey === "constructor") {
+                return;
+            }
+
             const descriptor = Object.getOwnPropertyDescriptor(target.prototype, propertyKey);
             if (descriptor && typeof descriptor.value === 'function') {
                 const originalMethod = descriptor.value;
+                const methodName = String(propertyKey);
                 descriptor.value = function (...args: any[]) {
-                    const span = tracer.startSpan(`${className}.${propertyKey}`);
-                    try {
-                        const result = originalMethod.apply(this, args);
-                        if (result instanceof Promise) {
-                            return result
-                                .then((res) => {
-                                    span.end();
-                                    return res;
-                                })
-                                .catch((err) => {
-                                    span.recordException(err);
-                                    span.end();
-                                    throw err;
-                                });
-                        } else {
+                    return tracer.startActiveSpan(`${className}.${methodName}`, (span) => {
+                        try {
+                            const result = originalMethod.apply(this, args);
+                            if (result && typeof result.then === "function") {
+                                return result
+                                    .then((res: unknown) => {
+                                        span.end();
+                                        return res;
+                                    })
+                                    .catch((err: unknown) => {
+                                        span.recordException(err as Error);
+                                        span.setStatus({ code: SpanStatusCode.ERROR });
+                                        span.end();
+                                        throw err;
+                                    });
+                            }
+
                             span.end();
                             return result;
+                        } catch (err: unknown) {
+                            span.recordException(err as Error);
+                            span.setStatus({ code: SpanStatusCode.ERROR });
+                            span.end();
+                            throw err;
                         }
-                    } catch (err: any) {
-                        span.recordException(err);
-                        span.end();
-                        throw err;
-                    }
+                    });
                 }
                 Object.defineProperty(target.prototype, propertyKey, descriptor);
             }
--- src/tracing.decorator.ts
+++ src/tracing.decorator.ts
@@ -1,34 +1,38 @@
-import { trace } from "@opentelemetry/api";
+import { SpanStatusCode, trace } from "@opentelemetry/api";
 
 export function Tracing(): MethodDecorator {
     return (target, propertyKey, descriptor: PropertyDescriptor) => {
         const className = target.constructor.name;
         const tracer = trace.getTracer(className);
         const originalMethod = descriptor.value;
+        const methodName = String(propertyKey);
         descriptor.value = function (...args: any[]) {
-            const span = tracer.startSpan(`${className}.${propertyKey.toString()}`);
-            try {
-                const result = originalMethod.apply(this, args);
-                if (result instanceof Promise) {
-                    return result
-                        .then((res) => {
-                            span.end();
-                            return res;
-                        })
-                        .catch((err) => {
-                            span.recordException(err);
-                            span.end();
-                            throw err;
-                        });
-                } else {
+            return tracer.startActiveSpan(`${className}.${methodName}`, (span) => {
+                try {
+                    const result = originalMethod.apply(this, args);
+                    if (result && typeof result.then === "function") {
+                        return result
+                            .then((res: unknown) => {
+                                span.end();
+                                return res;
+                            })
+                            .catch((err: unknown) => {
+                                span.recordException(err as Error);
+                                span.setStatus({ code: SpanStatusCode.ERROR });
+                                span.end();
+                                throw err;
+                            });
+                    }
+
                     span.end();
                     return result;
+                } catch (err: unknown) {
+                    span.recordException(err as Error);
+                    span.setStatus({ code: SpanStatusCode.ERROR });
+                    span.end();
+                    throw err;
                 }
-            } catch (err: any) {
-                span.recordException(err);
-                span.end();
-                throw err;
-            }
+            });
         }
     }
 }
